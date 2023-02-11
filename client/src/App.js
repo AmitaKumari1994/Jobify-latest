3bd2df95dba367dcaa838c6206d1a63b620cfa45
AmitaKumari1994 <80741439+AmitaKumari1994@users.noreply.github.com>
2023-02-12 01:28:17 +0530

    Error Page created

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import Landing from "./pages/Landing";
 import styled from "styled-components";
-import {BrowserRouter, Routes , Route , Link} from 'react-router-dom'
+import {BrowserRouter, Routes , Route } from 'react-router-dom'
+import {Dashboard,Error,Landing,Register} from '../src/pages/index'
 
 
 
@@ -8,16 +8,12 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <nav>
-          <Link to='/'>Dashboard</Link>
-          <Link to='/Register'>Register</Link>
-          <Link to='/Landing'>Landing</Link>
-        </nav>
+        
         <Routes>
-            <Route path='/' element={<div>Dashboard</div>}/>
-            <Route path='/Register' element={<div>Register</div>}/>
+            <Route path='/' element={<Dashboard/>}/>
+            <Route path='/Register' element={<Register/>}/>
             <Route path='/Landing' element={<Landing />}/>
-            <Route path='*' element={<h1>Error</h1>}/>
+            <Route path='*' element={<Error/>}/>
         </Routes>
       </BrowserRouter>
       
--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/ErrorPage'
+import img from '../assets/images/not-found.svg'
 
 const Error = () => {
   return (
-    <h1>Error</h1>
+    <Wrapper className='full-page'>
+        <div>
+            <img src={img} alt='Page not found'/>
+            <h3>Unable to find what you are looking for</h3>
+            <p>Please click the below button to return to the home page</p>
+            <Link to ='/' >Back home </Link>
+        </div>
+
+    </Wrapper>
   )
 }
 
