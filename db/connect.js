819c4bcd2976609493056ab0a3b059d0073785f6
AmitaKumari1994 <80741439+AmitaKumari1994@users.noreply.github.com>
2023-02-16 01:58:19 +0530

    server setup with mongo db completed

--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,8 @@
-const connectionString = 'mongodb+srv://abhiscience:<password>@abhinavcluster.b2daqtw.mongodb.net/abhiKart?retryWrites=true&w=majority'
+// const connectionString = 'mongodb+srv://abhiscience:<password>@abhinavcluster.b2daqtw.mongodb.net/abhiKart?retryWrites=true&w=majority'
+import mongoose from "mongoose";
+
+const connectDB = (url)=>{
+    return mongoose.connect(url)
+}
+
+export default connectDB
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
 
+import connectDB from './db/connect.js'
+
 
 import errorHandlerMiddleware from './middleware/error-handler.js'
 import notFoundMiddleware from './middleware/not-found.js'
@@ -19,6 +21,17 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware)
 const port = process.env.PORT || 5000;
 
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}...`)
-})
+
+
+const start =async ()=>{
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port,()=>{
+            console.log(`server is running on port ${port}...`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
